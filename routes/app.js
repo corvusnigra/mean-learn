e240cf637a9d3ec868efa9d52501f2b7f9e91532
ymarkov <itemruby@gmail.com>
2017-10-13 18:03:31 +0300

    add mongo connect

--- routes/app.js
+++ routes/app.js
@@ -5,8 +5,13 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
-router.get('/message', function (req, res, next) {
-    res.render('node', { message: 'Hello world !'});
+router.post('/message', function (req, res, next) {
+    const message = req.body.message
+    res.redirect('/message/' + message);
+});
+
+router.get('/message/:msg', function (req, res, next) {
+    res.render('node', { message: req.params.msg});
 });
 
 module.exports = router;
